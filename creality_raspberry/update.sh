2b9a4de119bfeae31a8bae1308734823d8b2d55d
yangchengwen <yangchengwen@creality.com>
2021-07-13 13:57:39 +0800

    delete qrencode \n

--- creality_raspberry/update.sh
+++ creality_raspberry/update.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 #taq=$1
+sudo git fetch --all
+sudo git reset --hard origin/main
 sudo git pull
+sudo systemctl restart creality_printer.service
+sudo systemctl restart creality_video_monitoring.service
 #git checkout "$tag" 
